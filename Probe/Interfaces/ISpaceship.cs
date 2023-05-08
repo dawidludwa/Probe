8c469f3e3c7b1063e21d3ea173fcf8023bbd9aab
Dawid <dawidludwa@gmail.com>
2023-05-08 17:21:54 +0200

    move to other module

---
+++ Probe/Interfaces/ISpaceship.cs
@@ -0,0 +1,15 @@
+﻿namespace Probe.Interfaces
+{
+    public interface ISpaceship
+    {
+        ICargo Cargo { get; }
+        ICrew Crew { get; }
+        IFuel Fuel { get; }
+        string Name { get; }
+        int Price { get; }
+        int Range { get; }
+        IShield Shield { get; }
+        double Speed { get; }
+        IWeapon Weapon { get; }
+    }
+}
---
+++ Reporter/RaportGen.cs
@@ -0,0 +1,13 @@
+﻿
+using Probe.Interfaces;
+
+namespace RaportGen
+{
+	internal class RaportGen
+	{
+		public void PrintDetails(ISpaceship spaceship)
+		{
+			Console.WriteLine(spaceship.Name);
+		}
+	}
+}
