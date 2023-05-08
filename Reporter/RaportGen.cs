725e80415012c556177279640cba5212f9ad7bf0
Dawid <dawidludwa@gmail.com>
2023-05-08 21:17:04 +0200

    add ut

--- Reporter/RaportGen.cs
+++ Reporter/RaportGen.cs
@@ -1,13 +1,25 @@
 ﻿
 using Probe.Interfaces;
+using System.Text;
 
 namespace RaportGen
 {
-	internal class RaportGen
+	public class RaportGen
 	{
-		public void PrintDetails(ISpaceship spaceship)
+		public string PrintDetails(ISpaceship spaceship)
 		{
-			Console.WriteLine(spaceship.Name);
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine(spaceship.Name);
+			sb.AppendLine("Crew: " + spaceship.Crew);
+			sb.AppendLine("Speed: " + spaceship.Speed);
+			sb.AppendLine("Range: " + spaceship.Range);
+			sb.AppendLine("Weapon: " + spaceship.Weapon);
+			sb.AppendLine("Shield: " + spaceship.Shield);
+			sb.AppendLine("Cargo: " + spaceship.Cargo);
+			sb.AppendLine("Fuel: " + spaceship.Fuel);
+			sb.AppendLine("Price: " + spaceship.Price);
+
+			return sb.ToString();
 		}
 	}
 }
